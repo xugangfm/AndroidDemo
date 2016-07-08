9c4c93bd4875548f16ed8e18b27de2be73d5cabe
xugang <xugangfm@163.com>
2016-07-09 06:50:00 +0800

    提交数据库

---
+++ src/com/xg/androiddemo/activity/test/DBTestActivity.java
@@ -0,0 +1,13 @@
+package com.xg.androiddemo.activity.test;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class DBTestActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dbtest);
+    }
+}
---
+++ src/com/xg/androiddemo/db/UserInfo.java
@@ -0,0 +1,7 @@
+package com.xg.androiddemo.db;
+
+/**
+ * Created by xugang on 16/7/8.
+ */
+public class UserInfo {
+}
